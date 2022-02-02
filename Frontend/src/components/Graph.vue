651f5dbda5a4e25dbf6bfd98ef178632be75b452
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2022-02-02 17:51:35 +0300

    Add files via upload

--- Frontend/src/components/Graph.vue
+++ Frontend/src/components/Graph.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="graph" >
-    <canvas id = "canvas" width="512" height="512">
+  <div class="graph">
+    <canvas id = "canvas" width="512" height="512" @click="submitGraph">
       <img ref="curImg" :src="selectedURL" alt="img loading failed" width="512" height="512"/>
     </canvas>
   </div>
@@ -11,7 +11,7 @@ export default {
   name: "Graph",
   props: {
     radius: {
-      type: String, Number,
+      type: Number,
     },
     dots: {
       type: Array,
@@ -22,39 +22,56 @@ export default {
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
     var img = this.$refs.curImg;
-    const list = this.dots;
+    var self = this;
+
     img.onload = function () {
       ctx.clearRect(0, 0, 512, 512);
       ctx.drawImage(img, 0, 0);
-
-      let dot = list[0];
-      console.log(dot.x+" "+dot.y+" "+dot.r+" "+dot.result+" "+dot.ctime+" "+dot.ptime);
-      dot = list[1];
-      console.log(dot.x+" "+dot.y+" "+dot.r+" "+dot.result+" "+dot.ctime+" "+dot.ptime);
-
+      Array.from(self.dots).forEach( dot => {
+        if (parseFloat(dot.r+"") === parseFloat(self.radius+"")){
+          if (dot.result === 'HIT'){
+            ctx.fillStyle = 'red';
+          } else {
+            ctx.fillStyle = 'lightgreen';
+          }
+          ctx.beginPath();
+          ctx.arc(255+dot.x*200, 255-dot.y*200, 4, 0, Math.PI * 2, false);
+          ctx.closePath();
+          ctx.fill();
+        }
+      })
     }
-
   },
   computed: {
     selectedURL() {
       switch (this.radius) {
-        case '0.5':
-          return require('../assets/graphR1.png');
-          break;
-        case '1':
+        case 0:
+          return require('../assets/graphR0.png');
+        case 0.5:
+          return require('../assets/graphR05.png');
+        case 1:
+          return require("../assets/graphR1.png");
+        case 1.5:
+          return require("../assets/graphR15.png");
+        case 2:
           return require("../assets/graphR2.png");
-          break;
-        case '1.5':
-          return require("../assets/graphR3.png");
-          break;
-        case '2':
-          return require("../assets/graphR4.png");
-          break;
         default:
           return require("../assets/DEF.png");
-          break;
       }
     },
+  },
+  methods: {
+    submitGraph(event){
+      if (this.radius === null) {
+        document.getElementById("errorDisplay").innerText = "Выберите радиус!";
+      } else {
+        document.getElementById("errorDisplay").innerText = "Ошибок нетОшибок нетОшибок нет";
+        let calculatedX = (event.offsetX-255)/200*this.radius;
+        let calculatedY = -(event.offsetY-255)/200*this.radius;
+        this.$emit('DotSubmit',calculatedX,calculatedY,this.radius);
+      }
+    },
+
   }
 }
 </script>
